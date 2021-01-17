171aafeb72e243c546ce5c15d453418b43e69395
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-17 17:10:51 +1100

    adjust AddHabit styling, add arrow to category item

--- components/addhabit/AddHabitMain.js
+++ components/addhabit/AddHabitMain.js
@@ -1,6 +1,5 @@
 import React, {useContext} from 'react';
 import {AddHabitContext} from '../contexts/AddHabitContext';
-import {useState} from 'react';
 import {
   View,
   Text,
@@ -15,8 +14,7 @@ const AddHabitMain = ({navigation}) => {
   const [habitDetails, setHabitDetails] = useContext(AddHabitContext);
 
   return (
-    <View>
-      <Text>Add Habit Main</Text>
+    <View style={styles.container}>
       <View style={styles.habitItem}>
         <MCIcon name="lead-pencil" color={'black'} size={26} />
         <TextInput
@@ -25,26 +23,45 @@ const AddHabitMain = ({navigation}) => {
             setHabitDetails({...habitDetails, name: text})
           }></TextInput>
       </View>
-      <Button
-        title="test"
-        onPress={() => {
-          console.log(habitDetails);
-        }}></Button>
+
       <TouchableOpacity
         onPress={() => navigation.navigate('test')}
         style={styles.habitItem}>
         <MCIcon name="apps" color={'black'} size={26} />
         <Text>{habitDetails.category}</Text>
+        <MCIcon
+          style={styles.rightIcon}
+          name="code-greater-than"
+          color={'black'}
+          size={26}
+        />
       </TouchableOpacity>
+      <Button
+        title="test"
+        onPress={() => {
+          console.log(habitDetails);
+        }}></Button>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: 17,
+  },
   habitItem: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+    height: 40,
+    marginBottom: 6,
+    borderBottomColor: 'lightgray',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  rightIcon: {
+    marginLeft: 'auto',
   },
 });
 
