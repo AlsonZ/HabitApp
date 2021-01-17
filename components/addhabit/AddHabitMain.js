dd623d7bb444e5fab4a6fdc47cb4487bbce07336
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-17 20:20:23 +1100

    create Description, Schedule items in addHabit

--- components/addhabit/AddHabitMain.js
+++ components/addhabit/AddHabitMain.js
@@ -9,6 +9,9 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
+import AntIcon from 'react-native-vector-icons/AntDesign';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
 
 const AddHabitMain = ({navigation}) => {
   const [habitDetails, setHabitDetails] = useContext(AddHabitContext);
@@ -16,19 +19,28 @@ const AddHabitMain = ({navigation}) => {
   return (
     <View style={styles.container}>
       <View style={styles.habitItem}>
-        <MCIcon name="lead-pencil" color={'black'} size={26} />
+        <MCIcon
+          style={styles.habitIcon}
+          name="lead-pencil"
+          color={'black'}
+          size={26}
+        />
         <TextInput
           placeholder="Habit Name"
           onChangeText={(text) =>
             setHabitDetails({...habitDetails, name: text})
           }></TextInput>
       </View>
-
       <TouchableOpacity
         onPress={() => navigation.navigate('test')}
         style={styles.habitItem}>
-        <MCIcon name="apps" color={'black'} size={26} />
-        <Text>{habitDetails.category}</Text>
+        <AntIcon
+          style={styles.habitIcon}
+          name="appstore-o"
+          color={'black'}
+          size={26}
+        />
+        <Text style={styles.habitText}>{habitDetails.category}</Text>
         <MCIcon
           style={styles.rightIcon}
           name="code-greater-than"
@@ -36,6 +48,36 @@ const AddHabitMain = ({navigation}) => {
           size={26}
         />
       </TouchableOpacity>
+      <View style={styles.habitItem}>
+        <AntIcon
+          style={styles.habitIcon}
+          name="filetext1"
+          color={'black'}
+          size={24}></AntIcon>
+        <TextInput
+          placeholder="Description"
+          onChangeText={(text) =>
+            setHabitDetails({...habitDetails, description: text})
+          }></TextInput>
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('test')}
+        style={styles.habitItem}>
+        <FontistoIcon
+          style={styles.habitIcon}
+          name="date"
+          color={'black'}
+          size={24}
+        />
+        <Text style={styles.habitText}>Schedule</Text>
+        <MCIcon
+          style={styles.rightIcon}
+          name="code-greater-than"
+          color={'black'}
+          size={26}
+        />
+      </TouchableOpacity>
+
       <Button
         title="test"
         onPress={() => {
@@ -55,10 +97,17 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    height: 40,
+    height: 44,
     marginBottom: 6,
     borderBottomColor: 'lightgray',
     borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  habitIcon: {
+    width: 35,
+    height: 30,
+  },
+  habitText: {
+    margin: 3,
   },
   rightIcon: {
     marginLeft: 'auto',
