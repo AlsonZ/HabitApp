e98c8322fe65d4283e2bef2c2e4f76ba2e579aba
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-17 15:57:16 +1100

    create Habit name item

--- components/addhabit/AddHabitMain.js
+++ components/addhabit/AddHabitMain.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {useState} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const AddHabitMain = ({navigation}) => {
+  const [habitName, setHabitName] = useState('');
+
   return (
     <View>
       <Text>Add Habit Main</Text>
+      <View style={styles.habitItem}>
+        <MCIcon name="lead-pencil" color={'black'} size={26} />
+        <TextInput
+          placeholder="Habit Name"
+          onChangeText={(text) => setHabitName(text)}></TextInput>
+      </View>
+
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('test')}
@@ -12,5 +24,13 @@ const AddHabitMain = ({navigation}) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  habitItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
 
 export default AddHabitMain;
