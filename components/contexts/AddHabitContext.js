5f4764fe47c9f9f803eac180cbcecfde3971938b
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-17 16:22:26 +1100

    create AddHabitContext, AddHabitProvider to store Habit state

---
+++ components/contexts/AddHabitContext.js
@@ -0,0 +1,28 @@
+import React, {createContext, useState} from 'react';
+import Colors from '../settings/Colors';
+
+export const AddHabitContext = createContext();
+
+export const AddHabitProvider = (props) => {
+  const [habitDetails, setHabitDetails] = useState({
+    name: '',
+    category: 'Category', // placeholder text
+    description: '',
+    schedule: 1, // 1 = daily
+    dailySchedule: 1, // 1 = once per day
+    colors: {
+      // default colors here
+      textColor: Colors.gray,
+      backgroundColor: Colors.transparent,
+      textActiveColor: Colors.white,
+      backgroundActiveColor: Colors.blue,
+    },
+    order: 0,
+  });
+
+  return (
+    <AddHabitContext.Provider value={[habitDetails, setHabitDetails]}>
+      {props.children}
+    </AddHabitContext.Provider>
+  );
+};
