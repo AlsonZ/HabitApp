49a65faa4a5d13a48746fedddae14492b0ad800b
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-20 16:16:26 +1100

    create ChooseCategoryIcon

---
+++ components/addhabit/items/ChooseCategoryIcon.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Colors from '../../settings/Colors';
+
+const Category = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        // onPress={() => navigation.navigate('Category')}
+        style={styles.habitItem}>
+        {/* <AntIcon
+          style={styles.habitIcon}
+          name="appstore-o"
+          color={'black'}
+          size={26}
+        /> */}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: 17,
+  },
+  habitItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 44,
+    marginBottom: 6,
+    borderBottomColor: 'lightgray',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  habitIcon: {
+    width: 35,
+    height: 25,
+  },
+  habitText: {
+    margin: 3,
+  },
+  rightIcon: {
+    marginLeft: 'auto',
+  },
+});
+
+export default Category;
