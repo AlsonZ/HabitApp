767011ee071847ec744065c3629e7ef7bc9d204e
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-27 23:15:55 +1100

    create loadHabits to load from HabitListContext

--- components/Habits.js
+++ components/Habits.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import HabitButton from './HabitButton';
 import {DefaultColors as Colors} from './settings/Colors';
+import {HabitListContext} from './contexts/HabitListContext';
 
 const Habits = () => {
+  const [habitList, setHabitList] = useContext(HabitListContext);
+
   const press = () => {};
+  const loadHabits = () => {
+    if (habitList) {
+      return habitList.map((habit, index) => (
+        <HabitButton
+          key={index + habit.name}
+          title={habit.name}
+          textColor={habit.colors.textColor}
+          backgroundColor={habit.colors.backgroundColor} // add styling to make this auto transparent and not dependent on the rgba here
+          textActiveColor={habit.colors.textActiveColor}
+          backgroundActiveColor={habit.colors.backgroundActiveColor}
+          onPress={() => {
+            console.log(`Pressed ${habit.name}`);
+          }}></HabitButton>
+      ));
+    }
+    // else return "create a new Habit now + navigation link to add habit screen"
+  };
   return (
     <View>
       <Button title="Test" style={styles.habitButton}></Button>
@@ -15,6 +35,7 @@ const Habits = () => {
         textActiveColor={Colors.white}
         backgroundActiveColor={Colors.blue}
         onPress={press}></HabitButton>
+      {loadHabits()}
     </View>
   );
 };
