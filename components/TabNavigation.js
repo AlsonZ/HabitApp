8de4631f85c9e6a20d18d8298e7af5ac76dfdba0
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-27 23:07:55 +1100

    change location of HabitListProvider to general TabNavigation

--- components/TabNavigation.js
+++ components/TabNavigation.js
@@ -6,6 +6,7 @@ import {DefaultColors as Colors} from './settings/Colors';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AddHabitNav from './addhabit/AddHabitNav';
+import {HabitListProvider} from './contexts/HabitListContext';
 const Tab = createMaterialBottomTabNavigator();
 
 const EditHabit = () => {
@@ -18,36 +19,40 @@ const EditHabit = () => {
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator initialRouteName="Home" inactiveColor={Colors.lightgray}>
-      <Tab.Screen
-        name="Home"
-        component={Habits}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({color}) => <Icon name="home" color={color} size={26} />,
-        }}
-      />
-      <Tab.Screen
-        name="Add"
-        component={AddHabitNav}
-        options={{
-          tabBarLabel: 'Add',
-          tabBarIcon: ({color}) => (
-            <MCIcon name="plus-circle-outline" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Edit"
-        component={EditHabit}
-        options={{
-          tabBarLabel: 'Edit',
-          tabBarIcon: ({color}) => (
-            <MCIcon name="lead-pencil" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <HabitListProvider>
+      <Tab.Navigator initialRouteName="Home" inactiveColor={Colors.lightgray}>
+        <Tab.Screen
+          name="Home"
+          component={Habits}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color}) => (
+              <Icon name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Add"
+          component={AddHabitNav}
+          options={{
+            tabBarLabel: 'Add',
+            tabBarIcon: ({color}) => (
+              <MCIcon name="plus-circle-outline" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Edit"
+          component={EditHabit}
+          options={{
+            tabBarLabel: 'Edit',
+            tabBarIcon: ({color}) => (
+              <MCIcon name="lead-pencil" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </HabitListProvider>
   );
 };
 
