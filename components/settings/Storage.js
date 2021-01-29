a754fd281917560fc330f8645f13d560b4665096
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-29 21:10:28 +1100

    create Storage.js for AsyncStorage Functions

---
+++ components/settings/Storage.js
@@ -0,0 +1,24 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeHabits = async (value) => {
+  try {
+    console.log('Storing: ' + value);
+    const jsonValue = JSON.stringify(value);
+    console.log('Storing JSON: ' + jsonValue);
+    await AsyncStorage.setItem('HabitList', jsonValue);
+  } catch (e) {
+    console.log('Storage STORE Error: ' + e);
+  }
+};
+export const getHabits = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('HabitList');
+    if (jsonValue != null) {
+      return jsonValue;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.log('Storage GET Error: ' + e);
+  }
+};
