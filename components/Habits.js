24b6e3c9b9b9a16eaa1b0c917d9b81e3cb16a22b
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-30 18:41:09 +1100

    create limited habit button load on home screen, placeholder days nottifier in home screen, fix habit button layout in home screen

--- components/Habits.js
+++ components/Habits.js
@@ -3,87 +3,62 @@ import {StyleSheet, View, Text, Button} from 'react-native';
 import HabitButton from './HabitButton';
 import {DefaultColors as Colors} from './settings/Colors';
 import {HabitListContext} from './contexts/HabitListContext';
+import ColorIcon from './addhabit/icons/ColorIcon';
 
 const Habits = () => {
   const [habitList, setHabitList] = useContext(HabitListContext);
 
-  const press = () => {};
-  const loadHabits = () => {
+  const loadHabits = (section) => {
     if (habitList) {
-      return habitList.map((habit, index) => (
-        <HabitButton
-          key={index + habit.name}
-          title={habit.name}
-          textColor={habit.colors.textColor}
-          backgroundColor={habit.colors.backgroundColor} // add styling to make this auto transparent and not dependent on the rgba here
-          textActiveColor={habit.colors.textActiveColor}
-          backgroundActiveColor={habit.colors.backgroundActiveColor}
-          onPress={() => {
-            console.log(`Pressed ${habit.name}`);
-          }}></HabitButton>
-      ));
+      let indents = [];
+      const habitsToLoad = 4;
+      const startHabitIndex = section == 'top' ? 0 : 4;
+      const maxLoaded = startHabitIndex + habitsToLoad;
+      const maximumLoadedHabits =
+        habitList.length > maxLoaded ? maxLoaded : habitList.length;
+      for (let i = startHabitIndex; i < maximumLoadedHabits; i++) {
+        indents.push(
+          <HabitButton
+            key={i + habitList[i].name}
+            title={habitList[i].name}
+            textColor={habitList[i].colors.textColor}
+            backgroundColor={habitList[i].colors.backgroundColor} // add styling to make this auto transparent and not dependent on the rgba here
+            textActiveColor={habitList[i].colors.textActiveColor}
+            backgroundActiveColor={habitList[i].colors.backgroundActiveColor}
+            onPress={() => {
+              console.log(`Pressed ${habitList[i].name}`);
+            }}></HabitButton>,
+        );
+      }
+      // return indents;
+      return <View style={[styles[section]]}>{indents}</View>;
     }
     // else return "create a new Habit now + navigation link to add habit screen"
   };
   return (
     <View style={styles.container}>
-      <HabitButton
-        title="Habit"
-        textColor={Colors.gray}
-        backgroundColor={Colors.transparent}
-        textActiveColor={Colors.white}
-        backgroundActiveColor={Colors.blue}
-        onPress={press}></HabitButton>
-      <HabitButton
-        title="Habit"
-        textColor={Colors.gray}
-        backgroundColor={Colors.transparent}
-        textActiveColor={Colors.white}
-        backgroundActiveColor={Colors.blue}
-        onPress={press}></HabitButton>
-      <HabitButton
-        title="Habit"
-        textColor={Colors.gray}
-        backgroundColor={Colors.transparent}
-        textActiveColor={Colors.white}
-        backgroundActiveColor={Colors.blue}
-        onPress={press}></HabitButton>
-      <HabitButton
-        title="Habit"
-        textColor={Colors.gray}
-        backgroundColor={Colors.transparent}
-        textActiveColor={Colors.white}
-        backgroundActiveColor={Colors.blue}
-        onPress={press}></HabitButton>
-      <HabitButton
-        title="Habit"
-        textColor={Colors.gray}
-        backgroundColor={Colors.transparent}
-        textActiveColor={Colors.white}
-        backgroundActiveColor={Colors.blue}
-        onPress={press}></HabitButton>
-      <HabitButton
-        title="Habit"
-        textColor={Colors.gray}
-        backgroundColor={Colors.transparent}
-        textActiveColor={Colors.white}
-        backgroundActiveColor={Colors.blue}
-        onPress={press}></HabitButton>
-      <HabitButton
-        title="Habit"
-        textColor={Colors.gray}
-        backgroundColor={Colors.transparent}
-        textActiveColor={Colors.white}
-        backgroundActiveColor={Colors.blue}
-        onPress={press}></HabitButton>
-      <HabitButton
-        title="Habit"
-        textColor={Colors.gray}
-        backgroundColor={Colors.transparent}
-        textActiveColor={Colors.white}
-        backgroundActiveColor={Colors.blue}
-        onPress={press}></HabitButton>
-      {loadHabits()}
+      {loadHabits('top')}
+      {loadHabits('bottom')}
+      <View style={styles.days}>
+        <View style={styles.top}>
+          <ColorIcon style={styles.icon} />
+          <ColorIcon style={styles.icon} />
+          <ColorIcon style={styles.icon} />
+          <ColorIcon style={styles.icon} />
+          <ColorIcon style={styles.icon} />
+          <ColorIcon style={styles.icon} />
+          <ColorIcon style={styles.icon} />
+        </View>
+        <View style={styles.bottom}>
+          <ColorIcon style={styles.icon} activeColor={'blue'} />
+          <ColorIcon style={styles.icon} activeColor={'blue'} />
+          <ColorIcon style={styles.icon} activeColor={'blue'} />
+          <ColorIcon style={styles.icon} activeColor={'blue'} />
+          <ColorIcon style={styles.icon} activeColor={'blue'} />
+          <ColorIcon style={styles.icon} activeColor={'blue'} />
+          <ColorIcon style={styles.icon} activeColor={'blue'} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -91,10 +66,33 @@ const Habits = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    // alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'lightgreen',
+  },
+  top: {
+    display: 'flex',
     flexDirection: 'row',
-    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginBottom: 'auto',
+  },
+  bottom: {
+    // flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 'auto',
+  },
+  days: {
+    display: 'flex',
+    flexDirection: 'column',
+    // flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    margin: 10,
   },
 });
 
