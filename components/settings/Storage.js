e1e21deb1178af9545fc38324d7aa15ea4b19775
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-01-31 23:52:49 +1100

    create set and get date for async storage

--- components/settings/Storage.js
+++ components/settings/Storage.js
@@ -21,3 +21,26 @@ export const getHabits = async () => {
     console.log('Storage GET Error: ' + e);
   }
 };
+
+export const setDate = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    console.log('Storing Date JSON: ' + jsonValue);
+    await AsyncStorage.setItem('HabitDate', jsonValue);
+  } catch (e) {
+    console.log('Storage STORE Error: ' + e);
+  }
+};
+
+export const getDate = async (value) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('HabitDate');
+    if (jsonValue != null) {
+      return jsonValue;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.log('Storage GET Error: ' + e);
+  }
+};
