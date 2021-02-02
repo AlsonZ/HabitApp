4e1d6a97c3b77b26150f950a1fa1a36f242e3edb
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-02-02 18:33:01 +1100

    modify schedule data for new habits

--- components/contexts/AddHabitContext.js
+++ components/contexts/AddHabitContext.js
@@ -8,7 +8,23 @@ export const AddHabitProvider = (props) => {
     name: '',
     category: 'Category', // placeholder text
     description: '',
-    schedule: 1, // 1 = daily
+    // schedule: 1, // 1 = daily
+    schedule: [
+      {day: 1, active: true},
+      {day: 2, active: true},
+      {day: 3, active: true},
+      {day: 4, active: true},
+      {day: 5, active: true},
+      {day: 6, active: true},
+      {day: 7, active: true},
+      {day: 8, active: true},
+      {day: 9, active: true},
+      {day: 10, active: true},
+      {day: 11, active: true},
+      {day: 12, active: true},
+      {day: 13, active: true},
+      {day: 14, active: true},
+    ],
     dailySchedule: 1, // 1 = once per day
     colors: {
       // default colors here
