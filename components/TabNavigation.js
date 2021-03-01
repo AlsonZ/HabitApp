2beadec83b2380cc07572e65f365204bc9b20b47
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-01 20:20:50 +1100

    move editHabitProvider to tabNavigator, remove placeholder editHabit component from tabNavigator

--- components/TabNavigation.js
+++ components/TabNavigation.js
@@ -8,51 +8,46 @@ import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import AddHabitNav from './addhabit/AddHabitNav';
 import EditHabitNav from './edithabit/EditHabitNav';
 import {HabitListProvider} from './contexts/HabitListContext';
+import {EditHabitProvider} from './contexts/EditHabitContext';
 const Tab = createMaterialBottomTabNavigator();
-
-const EditHabit = () => {
-  return (
-    <View>
-      <Text>Edit Habit</Text>
-    </View>
-  );
-};
 
 const TabNavigation = () => {
   return (
     <HabitListProvider>
-      <Tab.Navigator initialRouteName="Home" inactiveColor={Colors.lightgray}>
-        <Tab.Screen
-          name="Home"
-          component={Habits}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({color}) => (
-              <Icon name="home" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Add"
-          component={AddHabitNav}
-          options={{
-            tabBarLabel: 'Add',
-            tabBarIcon: ({color}) => (
-              <MCIcon name="plus-circle-outline" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Edit"
-          component={EditHabitNav}
-          options={{
-            tabBarLabel: 'Edit',
-            tabBarIcon: ({color}) => (
-              <MCIcon name="lead-pencil" color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      <EditHabitProvider>
+        <Tab.Navigator initialRouteName="Home" inactiveColor={Colors.lightgray}>
+          <Tab.Screen
+            name="Home"
+            component={Habits}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({color}) => (
+                <Icon name="home" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Add"
+            component={AddHabitNav}
+            options={{
+              tabBarLabel: 'Add',
+              tabBarIcon: ({color}) => (
+                <MCIcon name="plus-circle-outline" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Edit"
+            component={EditHabitNav}
+            options={{
+              tabBarLabel: 'Edit',
+              tabBarIcon: ({color}) => (
+                <MCIcon name="lead-pencil" color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </EditHabitProvider>
     </HabitListProvider>
   );
 };
--- components/edithabit/EditHabitNav.js
+++ components/edithabit/EditHabitNav.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import EditHabitMain from './EditHabitMain.js';
-import {EditHabitProvider} from '../contexts/EditHabitContext';
 import {CategoriesProvider} from '../contexts/CategoriesContext';
 import EditHabitItem from './EditHabitItem';
 import CategoryItem from '../items/CategoryItem';
@@ -12,30 +11,28 @@ const Stack = createStackNavigator();
 const EditHabitNav = () => {
   return (
     <CategoriesProvider>
-      <EditHabitProvider>
-        <Stack.Navigator initialRouteName="EditHabitMain">
-          <Stack.Screen
-            name="EditHabitMain"
-            options={{title: 'List of Habits'}}
-            component={EditHabitMain}
-          />
-          <Stack.Screen
-            options={{title: 'Edit Your Habit'}}
-            name="EditHabitItem"
-            component={EditHabitItem}
-          />
-          <Stack.Screen
-            options={{title: 'Categories'}}
-            name="EditCategories"
-            component={CategoryItem}
-          />
-          <Stack.Screen
-            options={{title: 'Select Colors'}}
-            name="EditHabitColors"
-            component={HabitColorItem}
-          />
-        </Stack.Navigator>
-      </EditHabitProvider>
+      <Stack.Navigator initialRouteName="EditHabitMain">
+        <Stack.Screen
+          name="EditHabitMain"
+          options={{title: 'List of Habits'}}
+          component={EditHabitMain}
+        />
+        <Stack.Screen
+          options={{title: 'Edit Your Habit'}}
+          name="EditHabitItem"
+          component={EditHabitItem}
+        />
+        <Stack.Screen
+          options={{title: 'Categories'}}
+          name="EditCategories"
+          component={CategoryItem}
+        />
+        <Stack.Screen
+          options={{title: 'Select Colors'}}
+          name="EditHabitColors"
+          component={HabitColorItem}
+        />
+      </Stack.Navigator>
     </CategoriesProvider>
   );
 };
