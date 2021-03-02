6aafe0d4028891dc8518a2b675198fb204482e5a
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-02 17:45:50 +1100

    add navigation to add new habit, fix landscape view, fix text input values

--- components/addhabit/AddHabitMain.js
+++ components/addhabit/AddHabitMain.js
@@ -11,6 +11,7 @@ import {
   TouchableOpacity,
   TouchableHighlight,
   View,
+  ScrollView,
 } from 'react-native';
 import ScheduleItem from '../items/ScheduleItem';
 
@@ -60,149 +61,156 @@ const AddHabitMain = ({navigation, route}) => {
 
   return (
     <View style={styles.container}>
-      <ModalItem
-        modalVisible={modalVisible}
-        setModalVisible={setModalVisible}
-        title={'Scheduled Days'}
-        modalHeader={true}
-        modalFooter={true}>
-        <FlatList
-          style={{marginRight: 1}}
-          data={habitDetails.schedule}
-          renderItem={({item, index}) => (
-            <ScheduleItem
-              index={index}
-              day={item.day}
-              active={item.active}
-              habitDetails={habitDetails}
-              setHabitDetails={setHabitDetails}
-            />
-          )}
-          keyExtractor={(item) => `${item.day}`}
-          extraData={habitDetails.schedule}
-        />
-      </ModalItem>
-      <View style={styles.habitItem}>
-        <MCIcon
-          style={styles.habitIcon}
-          name="lead-pencil"
-          color={'black'}
-          size={26}
-        />
-        <TextInput
-          placeholder="Habit Name"
-          onChangeText={(text) =>
-            setHabitDetails({...habitDetails, name: text})
-          }></TextInput>
-      </View>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate('Category', {
-            parentRoute: route.name,
-          })
-        }
-        style={styles.habitItem}>
-        <AntIcon
-          style={styles.habitIcon}
-          name="appstore-o"
-          color={'black'}
-          size={26}
-        />
-        <Text style={styles.habitText}>{habitDetails.category}</Text>
-        <MCIcon
-          style={styles.rightIcon}
-          name="code-greater-than"
-          color={'black'}
-          size={26}
-        />
-      </TouchableOpacity>
-      <View style={styles.habitItem}>
-        <AntIcon
-          style={styles.habitIcon}
-          name="filetext1"
-          color={'black'}
-          size={24}></AntIcon>
-        <TextInput
-          placeholder="Description"
-          onChangeText={(text) =>
-            setHabitDetails({...habitDetails, description: text})
-          }></TextInput>
-      </View>
-      <TouchableOpacity
-        onPress={() => {
-          setModalVisible(true);
-        }}
-        style={styles.habitItem}>
-        <FontistoIcon
-          style={styles.habitIcon}
-          name="date"
-          color={'black'}
-          size={24}
-        />
-        <Text style={styles.habitText}>Schedule</Text>
-        <MCIcon
-          style={styles.rightIcon}
-          name="code-greater-than"
-          color={'black'}
-          size={26}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('test')}
-        style={styles.habitItem}
-        disabled={true}>
-        <FontistoIcon
-          style={styles.habitIcon}
-          name="clock"
-          color={'gray'}
-          size={24}
-        />
-        <Text style={[styles.habitText, {color: 'gray'}]}>Daily Schedule</Text>
-        <NumberIcon
-          number={habitDetails.dailySchedule}
-          borderColor="gray"
-          textColor="gray"
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate('HabitColorItem', {
-            colors: habitDetails.colors,
-            parentRoute: route.name,
-          })
-        }
-        style={styles.habitItem}>
-        <IonIcon
-          style={styles.habitIcon}
-          name="color-palette-outline"
-          color={'black'}
-          size={24}
-        />
-        <Text style={styles.habitText}>Colors</Text>
-        <ColorIcon
-          activeColor={habitDetails.colors.backgroundActiveColor}
-          borderColor={Colors.border}
-          style={styles.colorIcon}
-        />
-      </TouchableOpacity>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <ModalItem
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+          title={'Scheduled Days'}
+          modalHeader={true}
+          modalFooter={true}>
+          <FlatList
+            style={{marginRight: 1}}
+            data={habitDetails.schedule}
+            renderItem={({item, index}) => (
+              <ScheduleItem
+                index={index}
+                day={item.day}
+                active={item.active}
+                habitDetails={habitDetails}
+                setHabitDetails={setHabitDetails}
+              />
+            )}
+            keyExtractor={(item) => `${item.day}`}
+            extraData={habitDetails.schedule}
+          />
+        </ModalItem>
+        <View style={styles.habitItem}>
+          <MCIcon
+            style={styles.habitIcon}
+            name="lead-pencil"
+            color={'black'}
+            size={26}
+          />
+          <TextInput
+            style={{flex: 1}}
+            placeholder="Habit Name"
+            value={habitDetails.name}
+            onChangeText={(text) =>
+              setHabitDetails({...habitDetails, name: text})
+            }></TextInput>
+        </View>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('Category', {
+              parentRoute: route.name,
+            })
+          }
+          style={styles.habitItem}>
+          <AntIcon
+            style={styles.habitIcon}
+            name="appstore-o"
+            color={'black'}
+            size={26}
+          />
+          <Text style={styles.habitText}>{habitDetails.category}</Text>
+          <MCIcon
+            style={styles.rightIcon}
+            name="code-greater-than"
+            color={'black'}
+            size={26}
+          />
+        </TouchableOpacity>
+        <View style={styles.habitItem}>
+          <AntIcon
+            style={styles.habitIcon}
+            name="filetext1"
+            color={'black'}
+            size={24}></AntIcon>
+          <TextInput
+            style={{flex: 1}}
+            placeholder="Description"
+            value={habitDetails.description}
+            onChangeText={(text) =>
+              setHabitDetails({...habitDetails, description: text})
+            }></TextInput>
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            setModalVisible(true);
+          }}
+          style={styles.habitItem}>
+          <FontistoIcon
+            style={styles.habitIcon}
+            name="date"
+            color={'black'}
+            size={24}
+          />
+          <Text style={styles.habitText}>Schedule</Text>
+          <MCIcon
+            style={styles.rightIcon}
+            name="code-greater-than"
+            color={'black'}
+            size={26}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('test')}
+          style={styles.habitItem}
+          disabled={true}>
+          <FontistoIcon
+            style={styles.habitIcon}
+            name="clock"
+            color={'gray'}
+            size={24}
+          />
+          <Text style={[styles.habitText, {color: 'gray'}]}>
+            Daily Schedule
+          </Text>
+          <NumberIcon
+            number={habitDetails.dailySchedule}
+            borderColor="gray"
+            textColor="gray"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('HabitColorItem', {
+              colors: habitDetails.colors,
+              parentRoute: route.name,
+            })
+          }
+          style={styles.habitItem}>
+          <IonIcon
+            style={styles.habitIcon}
+            name="color-palette-outline"
+            color={'black'}
+            size={24}
+          />
+          <Text style={styles.habitText}>Colors</Text>
+          <ColorIcon
+            activeColor={habitDetails.colors.backgroundActiveColor}
+            borderColor={Colors.border}
+            style={styles.colorIcon}
+          />
+        </TouchableOpacity>
 
-      {/* Reminders is not a feature currently enabled */}
-      <TouchableOpacity
-        onPress={() => navigation.navigate('test')}
-        style={styles.habitItem}
-        disabled={true}>
-        <IonIcon
-          style={styles.habitIcon}
-          name="notifications-outline"
-          color={'gray'}
-          size={24}
-        />
-        <Text style={[styles.habitText, {color: 'gray'}]}>Reminders</Text>
-        <NumberIcon number={`0`} borderColor="gray" textColor="gray" />
-      </TouchableOpacity>
+        {/* Reminders is not a feature currently enabled */}
+        <TouchableOpacity
+          onPress={() => navigation.navigate('test')}
+          style={styles.habitItem}
+          disabled={true}>
+          <IonIcon
+            style={styles.habitIcon}
+            name="notifications-outline"
+            color={'gray'}
+            size={24}
+          />
+          <Text style={[styles.habitText, {color: 'gray'}]}>Reminders</Text>
+          <NumberIcon number={`0`} borderColor="gray" textColor="gray" />
+        </TouchableOpacity>
 
-      {/* Order is only for Editing */}
-      {/* <TouchableOpacity
+        {/* Order is only for Editing */}
+        {/* <TouchableOpacity
         onPress={() => navigation.navigate('test')}
         style={styles.habitItem}>
         <IonIcon
@@ -215,38 +223,47 @@ const AddHabitMain = ({navigation, route}) => {
         <NumberIcon number={habitDetails.order} />
       </TouchableOpacity> */}
 
-      <Button
-        title="Create New Habit"
-        onPress={async () => {
-          const success = await storeNewHabit(habitDetails);
-          // const success = 'Success';
-          if (success === 'Success') {
-            // show modal popup for success with 3 buttons
-            // one to go to home, other to continue making duplicate habit
-            // one to start new
-            // reload Context's when new habit is added
-            setReloadContext(!reloadContext);
-            setReloadAddHabitContext(!reloadAddHabitContext);
-            setReloadEditContext(!reloadEditContext);
-          } else if (success === 'Name Matches Existing Habit') {
-            //show modal popup for error
-            console.log(success);
-          }
-        }}></Button>
-      <Button
-        title="Log Details"
-        onPress={async () => {
-          console.log(habitDetails);
-        }}></Button>
+        <Button
+          title="Create New Habit"
+          onPress={async () => {
+            const success = await storeNewHabit(habitDetails);
+            // const success = 'Success';
+            if (success === 'Success') {
+              // show modal popup for success with 3 buttons
+              // one to go to home, other to continue making duplicate habit
+              // one to start new
+              // reload Context's when new habit is added
+              setReloadContext(!reloadContext);
+              setReloadAddHabitContext(!reloadAddHabitContext);
+              setReloadEditContext(!reloadEditContext);
+              // send to main screen
+              navigation.navigate('Home');
+            } else if (success === 'Name Matches Existing Habit') {
+              //show modal popup for error
+              console.log(success);
+            }
+          }}></Button>
+        <Button
+          title="Log Details"
+          onPress={async () => {
+            console.log(habitDetails);
+          }}></Button>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 1,
+  },
+  scrollContainer: {
     display: 'flex',
     justifyContent: 'center',
-    margin: 17,
+    padding: 10,
   },
   habitItem: {
     display: 'flex',
