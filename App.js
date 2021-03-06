31f8488f175aae5f93d6ac3fdce3abc4038242e3
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-06 22:09:06 +1100

    add android navigation bar color change, statusbar color change, safeareaview change

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-} from 'react-native';
-
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet, StatusBar, Image} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigation from './components/TabNavigation';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 const App = () => {
+  useEffect(() => {
+    try {
+      changeNavigationBarColor('#123123');
+    } catch (e) {
+      console.log('Change Nav Error: ' + e);
+    }
+  }, []);
+
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{flex: 1}}>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar
+          translucent={true}
+          backgroundColor="transparent"
+          barStyle="dark-content"
+        />
+        {/* <SafeAreaView style={{flex: 1}}> */}
         <TabNavigation />
-      </SafeAreaView>
-    </NavigationContainer>
+        {/* </SafeAreaView> */}
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
