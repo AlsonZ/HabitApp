b3ca1271b04199be5560c7957904638f4812d2b5
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-10 20:32:10 +1100

    create calculateTime

--- components/Clock.js
+++ components/Clock.js
@@ -5,11 +5,54 @@ const Clock = () => {
   const [hour, setHour] = useState('00');
   const [minutes, setMinutes] = useState('00');
   const [seconds, setSeconds] = useState('00');
+  const [dayTime, setDayTime] = useState('pm');
+
+  const calculateTime = () => {
+    let date = new Date();
+    let hr = date.getUTCHours();
+    let min = date.getUTCMinutes();
+    let sec = date.getUTCSeconds();
+    let dt = date.getUTCHours();
+
+    if (min < 10) {
+      min = '0' + min;
+    }
+
+    if (sec < 10) {
+      sec = '0' + sec;
+    }
+
+    if (hr > 12) {
+      hr = hr - 12;
+    }
+
+    if (hr == 0) {
+      hr = 12;
+    }
+
+    if (`${hr}` !== hour) {
+      setHour(`${hr}`);
+    }
+    if (`${min}` !== minutes) {
+      setMinutes(`${min}`);
+    }
+    if (`${sec}` !== seconds) {
+      setSeconds(`${sec}`);
+    }
+    if (`${dt}` < 12) {
+      setDayTime('am');
+    } else {
+      setDayTime('pm');
+    }
+  };
 
   useEffect(() => {
-    // return () => {
-    //   cleanup;
-    // };
+    const timer = setInterval(() => {
+      calculateTime();
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
   }, []);
 
   return (
@@ -17,6 +60,7 @@ const Clock = () => {
       <Text style={styles.text}> {hour} :</Text>
       <Text style={styles.text}> {minutes} :</Text>
       <Text style={styles.text}> {seconds}</Text>
+      <Text style={styles.text}> {dayTime}</Text>
     </View>
   );
 };
