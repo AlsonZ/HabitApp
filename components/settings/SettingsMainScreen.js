e74b3bc5f276247159c393dca94e06195921d920
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-11 20:31:08 +1100

    create initial settingsmainscreen

---
+++ components/settings/SettingsMainScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SettingsMainScreen = () => {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+};
+
+export default SettingsMainScreen;
