b66c4829c2d1b85256ef14dbf87bbaa718e3603f
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-13 22:38:56 +1100

    create ThemeContext

---
+++ components/contexts/ThemeContext.js
@@ -0,0 +1,37 @@
+import React, {createContext, useEffect, useState} from 'react';
+import {getCurrentTheme, storeCurrentTheme} from '../settings/Storage';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = (props) => {
+  const [theme, setTheme] = useState({
+    bottomNavBarColor: 'black',
+    topNavBarColor: 'black',
+    highlightColor: 'lightBlue',
+    textColor: 'black',
+  });
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    let loading = false;
+    const getData = async () => {
+      loading = true;
+      const currentTheme = await getCurrentTheme();
+      setCategories(currentTheme);
+      loading = false;
+    };
+    if (!loading) {
+      getData();
+    }
+  }, [reload]);
+
+  const reloadContext = () => {
+    setReload(!reload);
+  };
+
+  return (
+    <ThemeContext.Provider value={[theme, setTheme, reloadContext]}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
