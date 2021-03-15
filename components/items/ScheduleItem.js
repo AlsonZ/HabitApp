6f1463e7ee532c3ed00e54d95d5ccaea34ca872c
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-16 02:32:52 +1100

    create initial scheduleItem, durationItem, scrollview picker, scheduleButton

--- components/items/ScheduleItem.js
+++ components/items/ScheduleItem.js
@@ -1,38 +1,136 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  ScrollView,
+} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import {DefaultColors} from '../settings/Colors';
+import {config} from '../config/config';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const ScheduleItem = ({day, active, habitDetails, setHabitDetails, index}) => {
+const ScheduleItem = () => {
+  // set this to scheduletype from navigation and not default to the config
+  const [active, setActive] = useState(config.scheduleType.everyday);
+  const [currentlyChosen, setCurrentlyChosen] = useState(0);
+  const durationPickerScrollView = useRef(null);
+  const durationItemHeight = 40;
+  const durations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const onPress = (val) => {
-    setHabitDetails((prevState) => {
-      const scheduleCopy = prevState.schedule;
-      scheduleCopy[index].active = val;
+    // setHabitDetails((prevState) => {
+    //   const scheduleCopy = prevState.schedule;
+    //   scheduleCopy[index].active = val;
+    //   return {
+    //     ...prevState,
+    //     schedule: scheduleCopy,
+    //   };
+    // });
+  };
 
-      return {
-        ...prevState,
-        schedule: scheduleCopy,
-      };
-    });
+  const ScheduleButton = ({scheduleType}) => {
+    return (
+      <TouchableHighlight
+        underlayColor={DefaultColors.touchableHightlightUnderlay}
+        onPress={() => {
+          setActive(scheduleType);
+        }}>
+        <View style={styles.container}>
+          <CheckBox
+            disable={false}
+            value={scheduleType === active}
+            onChange={() => {
+              setActive(scheduleType);
+            }}
+          />
+          <Text style={styles.checkBoxText}>{scheduleType.name}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  };
+  const DurationItem = ({text, index}) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          setCurrentlyChosen(durations[index]);
+          durationPickerScrollView.current.scrollTo({
+            y: index * durationItemHeight,
+            animated: true,
+          });
+        }}
+        style={{height: durationItemHeight}}>
+        <Text
+          style={{
+            borderColor: 'black',
+            borderWidth: 1,
+            textAlign: 'center',
+            fontSize: 20,
+            height: durationItemHeight,
+            lineHeight: durationItemHeight - durationItemHeight / 8,
+          }}>
+          {text}
+        </Text>
+      </TouchableOpacity>
+    );
   };
 
   return (
-    <TouchableHighlight
-      underlayColor={DefaultColors.touchableHightlightUnderlay}
-      onPress={() => {
-        onPress(!active);
-      }}>
-      <View style={styles.container}>
-        <CheckBox
-          disable={false}
-          value={active}
-          onValueChange={(val) => {
-            onPress(val);
-          }}
-        />
-        <Text style={styles.checkBoxText}>Day {day}</Text>
+    <View style={{flex: 1}}>
+      <View>
+        {Object.keys(config.scheduleType).map((key) => {
+          return (
+            <ScheduleButton
+              key={config.scheduleType[key].name}
+              scheduleType={config.scheduleType[key]}
+            />
+          );
+        })}
       </View>
-    </TouchableHighlight>
+      <View>
+        <Text>{currentlyChosen}</Text>
+      </View>
+      {active !== config.scheduleType.everyday && (
+        <View
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: durationItemHeight * 3,
+            width: 100,
+            marginTop: durationItemHeight,
+            backgroundColor: 'red',
+          }}>
+          <View
+            style={{
+              position: 'absolute',
+              height: durationItemHeight,
+              backgroundColor: 'rgba(255,1,1,0.5)',
+              width: '100%',
+              top: durationItemHeight,
+              borderRadius: 10,
+            }}
+          />
+          <ScrollView
+            ref={durationPickerScrollView}
+            snapToInterval={durationItemHeight}
+            showsVerticalScrollIndicator={false}
+            style={{
+              width: '100%',
+              paddingTop: durationItemHeight,
+            }}>
+            {durations.map((duration, index) => (
+              <DurationItem
+                key={duration + index}
+                text={duration}
+                index={index}
+              />
+            ))}
+            <View style={{height: durationItemHeight * 2}}></View>
+          </ScrollView>
+        </View>
+      )}
+    </View>
   );
 };
 
