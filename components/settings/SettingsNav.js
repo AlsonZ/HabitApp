70cf27b8adcb13bbbba4475a48aa506648313a5e
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-22 21:33:17 +1100

    add theme to SettingsNav header and headerTint

--- components/settings/SettingsNav.js
+++ components/settings/SettingsNav.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import SettingsMainScreen from './SettingsMainScreen';
 import SettingsThemeScreen from './screens/SettingsThemeScreen';
 import SettingsHabitButtonScreen from './screens/SettingsHabitButtonScreen';
 import HabitColorItem from '../items/HabitColorItem';
+import {ThemeContext} from '../contexts/ThemeContext';
 
 const Stack = createStackNavigator();
 
 const SettingsNav = () => {
+  const [theme, setTheme] = useContext(ThemeContext);
   return (
-    <Stack.Navigator headerMode="screen">
+    <Stack.Navigator
+      headerMode="screen"
+      screenOptions={{
+        headerStyle: {backgroundColor: theme.navTopBarColor},
+        headerTintColor: theme.navTopBarTextColor,
+      }}>
       <Stack.Screen
         name="SettingsMainScreen"
         options={{title: 'Settings', headerTransparent: false}}
