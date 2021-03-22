a0a7c23a80cee257c9f02096ef49f9b76267f58b
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-22 21:31:58 +1100

    add theme to AddHabitNav header and headerTint

--- components/addhabit/AddHabitNav.js
+++ components/addhabit/AddHabitNav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet} from 'react-native';
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import AddHabitMain from './AddHabitMain.js';
@@ -9,17 +9,23 @@ import CategoryItem from '../items/CategoryItem';
 import ScheduleItem from '../items/ScheduleItem';
 import HabitColorItem from '../items/HabitColorItem';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {ThemeContext} from '../contexts/ThemeContext';
 
 const Stack = createStackNavigator();
 
 const AddHabitNav = () => {
   const insets = useSafeAreaInsets();
+  const [theme, setTheme] = useContext(ThemeContext);
   return (
     <CategoriesProvider>
       <AddHabitProvider>
         <Stack.Navigator
           headerMode="screen"
-          screenOptions={{headerStatusBarHeight: insets.top}}>
+          screenOptions={{
+            headerStatusBarHeight: insets.top,
+            headerStyle: {backgroundColor: theme.navTopBarColor},
+            headerTintColor: theme.navTopBarTextColor,
+          }}>
           <Stack.Screen
             name="AddHabitMain"
             options={{title: 'Create a new Habit', headerTransparent: false}}
