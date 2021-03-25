fb43c10ae9d4b0f0951bd25fed07ccc56a8a7a9b
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-25 20:13:14 +1100

    add theme into App component to remove white flashes in dark theme

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigation from './components/TabNavigation';
-import {ThemeProvider} from './components/contexts/ThemeContext';
+import {ThemeProvider, ThemeContext} from './components/contexts/ThemeContext';
+
+const HabitApp = () => {
+  const [theme] = useContext(ThemeContext);
+  const MyTheme = {
+    dark: true,
+    colors: {
+      primary: theme.highlightColor,
+      background: theme.backgroundColor,
+      card: 'rgb(0,0,0)',
+      text: 'rgb(255,255,255)',
+      border: 'rgb(0,0,0)',
+      notification: 'rgb(0,0,0)',
+    },
+  };
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <TabNavigation />
+    </NavigationContainer>
+  );
+};
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <ThemeProvider>
-          <TabNavigation />
-        </ThemeProvider>
-      </NavigationContainer>
+      <ThemeProvider>
+        <HabitApp />
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 };
