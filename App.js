af7acbd3fdf22fe19e6f6e6eae84e9b3d6031759
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-28 20:59:35 +1100

    refactor App.js to suit new project structure

--- App.js
+++ App.js
@@ -1,41 +1,17 @@
 import 'react-native-gesture-handler';
-import React, {useContext} from 'react';
-import {StyleSheet, StatusBar} from 'react-native';
+import React from 'react';
+import {StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {NavigationContainer} from '@react-navigation/native';
-import TabNavigation from './components/TabNavigation';
-import {ThemeProvider, ThemeContext} from './components/contexts/ThemeContext';
-
-const HabitApp = () => {
-  const [theme] = useContext(ThemeContext);
-  const MyTheme = {
-    dark: true,
-    colors: {
-      primary: theme.highlightColor,
-      background: theme.backgroundColor,
-      card: 'rgb(0,0,0)',
-      text: 'rgb(255,255,255)',
-      border: 'rgb(0,0,0)',
-      notification: 'rgb(0,0,0)',
-    },
-  };
-  return (
-    <NavigationContainer theme={MyTheme}>
-      <TabNavigation />
-    </NavigationContainer>
-  );
-};
+import {ThemeProvider} from './components/contexts/ThemeContext';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <ThemeProvider>
-        <HabitApp />
+        <RootNavigation />
       </ThemeProvider>
     </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
