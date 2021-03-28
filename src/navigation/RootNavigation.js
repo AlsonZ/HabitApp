a27a5cd9a5855a78862ac9c113945f505daf0f1f
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-28 21:49:04 +1100

    create new RootNavigation

---
+++ src/navigation/RootNavigation.js
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import TabNavigation from './BottomNavigation';
+
+const RootNavigation = () => {
+  // const [theme] = useContext(ThemeContext);
+  // const MyTheme = {
+  //   dark: true,
+  //   colors: {
+  //     primary: theme.highlightColor,
+  //     background: theme.backgroundColor,
+  //     card: 'rgb(0,0,0)',
+  //     text: 'rgb(255,255,255)',
+  //     border: 'rgb(0,0,0)',
+  //     notification: 'rgb(0,0,0)',
+  //   },
+  // };
+  return (
+    <NavigationContainer>
+      <StatusBar
+      // translucent={true}
+      // backgroundColor={theme.navStatusBarColor}
+      // barStyle={theme.navStatusBarTextColor}
+      />
+      <BottomNavigation />
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigation;
