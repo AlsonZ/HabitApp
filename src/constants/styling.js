6d551ba3c0298eddf7371a2f7b32990fb6cf8cac
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-28 21:49:51 +1100

    create inital darkTheme for styling colors

---
+++ src/constants/styling.js
@@ -0,0 +1,11 @@
+export const colors = {
+  darkTheme: {
+    text: '#fff',
+    primary: '#0b0e11',
+    primary2: '#151a21',
+    secondary: 'red', //temp
+    accent: 'lightblue', //temp
+    border: '#000000',
+  },
+  lightTheme: {},
+};
