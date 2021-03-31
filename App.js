1c882f6b5569c291487c0f6f5452e90677241c18
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2021-03-31 23:41:06 +1100

    fix ThemeProvider import

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ThemeProvider} from './components/contexts/ThemeContext';
+import {ThemeProvider} from './src/contexts/ThemeContext';
 import RootNavigation from './src/navigation/RootNavigation';
 
 const App = () => {
